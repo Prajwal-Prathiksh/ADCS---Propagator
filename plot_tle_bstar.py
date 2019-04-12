338248e36d9414608f14481093981f3117c13de2
Jamun-Fanatic-Foreva <47190367+Jamun-Fanatic-Foreva@users.noreply.github.com>
2019-04-12 13:04:39 +0530

    Update plot_tle_bstar.py
    Changed the way the 3D-scatter plots are being generated.

--- plot_tle_bstar.py
+++ plot_tle_bstar.py
@@ -5,39 +5,56 @@ import matplotlib.pyplot as plt
 import testcase_satellite as tc
 from mpl_toolkits.mplot3d import Axes3D
 import epoch_drag_analysis as eda
+from mayavi import mlab
 
 
 Radius_Earth = 6378.1363
 fig = plt.figure()
 ax = fig.add_subplot(111, projection = '3d')
+ax.set_xlabel(r'x-axis $\rightarrow$')
+ax.set_ylabel(r'y-axis $\rightarrow$')
+ax.set_zlabel(r'z-axis $\rightarrow$')
 
-def plots(ch):
-	if ch == 1:
-		p = ax.scatter(eda.rx_list, eda.ry_list, eda.rz_list, marker = 'o',c = eda.drag_list)
-
-		plt.show(p)
-		fig.colorbar(p)
-		
-		
+def plots(ch, i):
+    '''
+    :ch: choice number
+    
+    :i: mask_points
+    '''
+    
+    if ch == 1:
+        x, y, z = eda.rx_list[::i], eda.ry_list[::i], eda.rz_list[::i]
+        b_list = eda.drag_list[::i]
+        
+        ax.scatter(x,y,z, marker = 'o',c = b_list)
+        fig.colorbar(ax.scatter(x,y,z, marker = 'o',c = b_list))
+        plt.show()
+        ax.set_aspect("equal")
+        
+        
+    elif ch == 2:
+        x, y, z = eda.rx_list[::i], eda.ry_list[::i], eda.rz_list[::i]
+        b_list = eda.drag_list[::i]
+        
+        ax.scatter(x,y,z, marker = 'o',c = b_list)
+        fig.colorbar(ax.scatter(x,y,z, marker = 'o',c = b_list))
+        plt.show()      
+        
+        u,v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+        x = Radius_Earth*np.cos(u)*np.sin(v)
+        y = Radius_Earth*np.sin(u)*np.sin(v)
+        z = Radius_Earth*np.cos(v)
+        ax.plot_wireframe(x,y,z)        
+        ax.set_aspect("equal")
 	
-	elif ch == 2:
-		p = ax.scatter(eda.rx_list, eda.ry_list, eda.rz_list, marker = 'o',c = eda.drag_list) 
-		plt.show(p)
-		fig.colorbar(p)
 
-		u,v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-		x = Radius_Earth*np.cos(u)*np.sin(v)
-		y = Radius_Earth*np.sin(u)*np.sin(v)
-		z = Radius_Earth*np.cos(v)
-		ax.plot_wireframe(x,y,z)
+'''
+pts = mlab.points3d(eda.rx_list, eda.ry_list, eda.rz_list,eda.drag_list, mask_points = 2)
+phi, theta = np.mgrid [0:np.pi:30j, 0:2*np.pi:20j]
+x = Radius_Earth*np.sin(phi)*np.cos(theta)
+y = Radius_Earth*np.sin(phi)*np.sin(theta)
+z = Radius_Earth*np.cos(phi)
 
-		ax.set_aspect("equal")
-		plt.show()
-
-
-ch = int(input('Enter Choice:'))
-plots(ch)
-
-
-
-
+mlab.mesh(x,y,z,representation = 'wireframe')
+mlab.show()
+'''
